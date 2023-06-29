c75437e775b14f8840db3157bc338bd1ffe14652
Kaio Melo dos Santos <kaiomelo1998@gmail.com>
2023-06-29 17:11:47 -0300

    Atualizando modulo 8 de OOP

--- Udemy/08 - Introdução à Progamação Orientada a Objeto/Exemplo/produtoEstoque/src/application/Program.java
+++ Udemy/08 - Introdução à Progamação Orientada a Objeto/Exemplo/produtoEstoque/src/application/Program.java
@@ -1,42 +1,43 @@
-package applicarion;
+package application;
+
+import entities.Product;
 
 import java.util.Locale;
 import java.util.Scanner;
-
-import entities.Product;
 
 public class Program {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
-
         Product product = new Product();
+
         System.out.println("Enter product data: ");
         System.out.print("Name: ");
         product.name = sc.nextLine();
         System.out.print("Price: ");
         product.price = sc.nextDouble();
-        System.out.print("Quantity in stock: ");
+        System.out.print("Quantity: ");
         product.quantity = sc.nextInt();
 
         System.out.println();
-        System.out.println("Product data: " + product);
+        System.out.println(product);
 
         System.out.println();
-        System.out.print("Enter the number of products to be added in stock: ");
+        System.out.println("Enter the number od products to be added in stock: ");
         int quantity = sc.nextInt();
         product.addProducts(quantity);
 
         System.out.println();
-        System.out.println("Updated data: " + product);
+        System.out.println("Update data: " + product);
 
         System.out.println();
-        System.out.print("Enter the number of products to be removed from stock: ");
+        System.out.println("Enter the number od products to be removed in stock: ");
         quantity = sc.nextInt();
         product.removeProducts(quantity);
 
         System.out.println();
-        System.out.println("Updated data: " + product);
+        System.out.println("Update data: " + product);
+
         sc.close();
     }
 }
--- Udemy/08 - Introdução à Progamação Orientada a Objeto/Exemplo/produtoEstoque/src/entities/Product.java
+++ Udemy/08 - Introdução à Progamação Orientada a Objeto/Exemplo/produtoEstoque/src/entities/Product.java
@@ -1,25 +1,30 @@
 package entities;
 
 public class Product {
+
     public String name;
     public double price;
     public int quantity;
-    public double totalValueInStock() {
+
+    public double totalValueInStock(){
         return price * quantity;
-    }
-    public void addProducts(int quantity) {
+    };
+
+    public void addProducts(int quantity){
         this.quantity += quantity;
-    }
-    public void removeProducts(int quantity) {
+    };
+
+    public void removeProducts(int quantity){
         this.quantity -= quantity;
-    }
-    public String toString() {
+    };
+
+    public String toString(){
         return name
-                + ", $ "
+                + ", $"
                 + String.format("%.2f", price)
                 + ", "
                 + quantity
-                + " units, Total: $ "
+                + " units, total: $ "
                 + String.format("%.2f", totalValueInStock());
-    }
+    };
 }
