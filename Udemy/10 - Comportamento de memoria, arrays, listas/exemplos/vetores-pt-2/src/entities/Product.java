191ff0ddf4c7bfbee28d541c81b8642342ccb998
Kaio Melo dos Santos <kaiomelo1998@gmail.com>
2023-07-03 17:43:24 -0300

    Update Product.java

--- Udemy/10 - Comportamento de memoria, arrays, listas/exemplos/vetores-pt-2/src/entities/Product.java
+++ Udemy/10 - Comportamento de memoria, arrays, listas/exemplos/vetores-pt-2/src/entities/Product.java
@@ -5,6 +5,9 @@ public class Product {
     public String name;
     public double price;
 
+    public Product() {
+    }
+
     public Product(String name, double price){
         this.name = name;
         this.price = price;
