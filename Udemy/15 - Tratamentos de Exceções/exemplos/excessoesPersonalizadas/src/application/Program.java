14c8cb674615b0bfb68d151f76ac8ebd728a5fbd
Kaio Melo dos Santos <kaiomelo1998@gmail.com>
2023-07-13 18:11:02 -0300

    versão projeto Exception ruim

--- Udemy/15 - Tratamentos de Exceções/exemplos/excessoesPersonalizadas/src/application/Program.java
+++ Udemy/15 - Tratamentos de Exceções/exemplos/excessoesPersonalizadas/src/application/Program.java
@@ -19,11 +19,11 @@ public class Program {
         System.out.println("Check-out date (dd/MM/yyyy): ");
         Date checkOut = sdf.parse(sc.next());
 
-        if(!checkOut.after(checkIn)){ // "after" verifica se check-out está depois de check-in
+        if (!checkOut.after(checkIn)) { // "after" verifica se check-out está depois de check-in
             System.out.println("Error in reservation: check-out date must be after check-in date");
-        }else{
+        } else {
             Reservation reservation = new Reservation(number, checkIn, checkOut);
-            System.out.println("Reservation: "+ reservation);
+            System.out.println("Reservation: " + reservation);
 
             System.out.println();
             System.out.println("Room number: ");
@@ -33,14 +33,11 @@ public class Program {
             System.out.println("Check-out date (dd/MM/yyyy): ");
             checkOut = sdf.parse(sc.next());
 
-            Date now = new Date();
-            if(checkIn.before(now) || checkOut.before(now)){
-                System.out.println("Error in reservation dates for updates must be future ");
-            }else if(!checkOut.after(checkIn)){
-                System.out.println("Error in reservation: check-out date must be after check-in date");
-            }else{
-                reservation.updateDates(checkIn, checkOut);
-                System.out.println("Reservation: "+ reservation);
+            String error = reservation.updateDates(checkIn, checkOut);
+            if (error != null) {
+                System.out.println("Error in reservation: " + error);
+            } else {
+                System.out.println("Reservation: " + reservation);
             }
         }
 
